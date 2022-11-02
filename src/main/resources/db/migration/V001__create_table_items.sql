07121d5c7bcde8e15c0e47b04323fd60e8ca0725
Rikatob <rikatob@hotmail.com>
2022-11-02 16:53:45 +0100

    Set up first version of flyway migration for sql DB * Changed application.template.properties values to fit local.

--- src/main/resources/db/migration/V001__create_table_items.sql
+++ src/main/resources/db/migration/V001__create_table_items.sql
@@ -6,3 +6,38 @@ CREATE TABLE items
     category    varchar(100),
     description varchar(100)
 );
+
+
+CREATE TABLE users
+(
+    id            int identity primary key NOT NULL,
+    username      varchar(20),
+    first_name    varchar(20),
+    last_name     varchar(20),
+    gender        varchar(20),
+    date_of_birth DATETIME
+);
+
+CREATE TABLE threads
+(
+    id int primary key not null
+);
+
+CREATE TABLE messages
+(
+    id        int identity primary key NOT NULL,
+    sent_date DATETIME,
+    body      varchar(1000),
+    user_id   int FOREIGN KEY REFERENCES users (id),
+    thread_id int FOREIGN KEY REFERENCES threads (id)
+
+);
+
+--> Linking entity
+CREATE TABLE thread_members
+(
+    thread_id int NOT NULL FOREIGN KEY REFERENCES threads (id),
+    user_id   int NOT NULL FOREIGN KEY REFERENCES users (id),
+    PRIMARY KEY (thread_id, user_id)
+)
+
