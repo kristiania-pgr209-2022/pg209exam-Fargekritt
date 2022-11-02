b60f7b35e9aecaf0eea23d9842824d006a075e0c
Rikatob <rikatob@hotmail.com>
2022-11-02 22:16:41 +0100

    Added a way to serialize and deserialize LocalDate. * Created LocalDateDeserializer and LocalDateSerializer * Created LocalDateAttributeConverter (dont need this for now, but may be needing this later for converting sql date) * added dependencies  * jackson-core  * jackson-annotations  * jackson-databind * USE jackson ObjectMapper to map User to json string in ChatRoomServerTest.

--- src/test/java/org/kristiania/chatRoom/ChatRoomServerTest.java
+++ src/test/java/org/kristiania/chatRoom/ChatRoomServerTest.java
@@ -1,5 +1,6 @@
 package org.kristiania.chatRoom;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.json.Json;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -71,22 +73,31 @@ public class ChatRoomServerTest {
 
     @Test
     void shouldAddAndListUser() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        User user = sampleUser();
+        String Userjson = mapper.writeValueAsString(user);
+
+        System.out.println(Userjson);
+
         var postConnection = openConnection("/api/users");
         postConnection.setRequestMethod("POST");
         postConnection.setRequestProperty("Content-Type", "application/json");
         postConnection.setDoOutput(true);
-        postConnection.getOutputStream().write(
-                Json.createObjectBuilder()
-                        .add("username", "TestUser")
-                        .add("firstName", "Firsty")
-                        .add("lastName", "Lasty")
-                        .add("gender", "male")
-                       /* .add("dateOfBirth", Json.createObjectBuilder()
-                                .add("year", "1999")
-                                .add("month","01")
-                                .add("dayOfMonth","01"))*/
-                        .build().toString().getBytes(StandardCharsets.UTF_8)
-        );
+        postConnection.getOutputStream().write(Userjson.getBytes(StandardCharsets.UTF_8));
+
+
+//        // Old way to send json.
+//       postConnection.getOutputStream().write(
+//                Json.createObjectBuilder()
+//                        .add("username", "TestUser")
+//                        .add("firstName", "Firsty")
+//                        .add("lastName", "Lasty")
+//                        .add("gender", "male")
+//                        .add("dateOfBirth","2012-01-20")
+//                        .build().toString().getBytes(StandardCharsets.UTF_8));
+//
+
 
         assertThat(postConnection.getResponseCode())
                 .as(postConnection.getResponseMessage() + " for " + postConnection.getURL())
@@ -100,15 +111,26 @@ public class ChatRoomServerTest {
         assertThat(connection.getInputStream())
                 .asString(StandardCharsets.UTF_8)
                 .contains(""" 
-                        firstName":"Firsty","gender":"male","id":1,"lastName":"Lasty","messages":[],"username":"TestUser""");
+                        dateOfBirth":"2012-01-20","firstName":"Bob","gender":"male","id":1,"lastName":"Kåre","messages":[],"username":"Lulu""");
 
     }
 
-/*    @Test
-    void shouldGetUserByUserName(){
+    /*    @Test
+        void shouldGetUserByUserName(){
 
-    }*/
+        }*/
     private HttpURLConnection openConnection(String spec) throws IOException {
         return (HttpURLConnection) new URL(server.getURL(), spec).openConnection();
+    }
+
+
+    private User sampleUser() {
+        var user = new User();
+        user.setUsername("Lulu");
+        user.setFirstName("Bob");
+        user.setLastName("Kåre");
+        user.setGender("male");
+        user.setDateOfBirth(LocalDate.of(2012, 1, 20));
+        return user;
     }
 }
