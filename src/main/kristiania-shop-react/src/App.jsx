36aea49442b1cacba262e789a5abc6d60b86bd1e
Rikatob <rikatob@hotmail.com>
2022-11-12 20:10:35 +0100

    REACT * Show frontPage * Created ListMessages * Created ListThreads * Created AddThread  * Only created, values copied AddUser, NOT WORKING * Created AddMessage  * NOT IMPLEMENTED * CLICK:   -> List users(ok)    -> List Threads of user(ok)     -> List Messages in Thread (started)

--- src/main/kristiania-shop-react/src/App.jsx
+++ src/main/kristiania-shop-react/src/App.jsx
@@ -2,7 +2,8 @@ import {useEffect, useState} from 'react'
 import './App.css'
 import {HashRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
 
-function ListUser() {
+function ListUser({userId}) {
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState([]);
 
@@ -21,16 +22,27 @@ function ListUser() {
     if (loading) {
         return <div>Loading...</div>
     }
-    return <div>
+
+    function handleClick(i) {
+        userId(i);
+        navigate("/threads")
+    }
+
+    return <div style={{width: '50vw', height: '50vh'}}>
+        <h1>Log in with a user</h1>
         <Link to={"/"}>Back to home</Link>
         <div>
             {user.map((i) => (
-                <ul>
-                    <li>{i.firstName}</li>
-                    <li>{i.lastName}</li>
-                    <li>{i.username}</li>
-                    <li>{i.gender}</li>
-                </ul>
+                <button value={i} onClick={() => handleClick(i)} style={{border: '1px solid black'}}>
+                    <dl>
+                        <dt>UserName:</dt>
+                        <dd>- {i.username}</dd>
+                        <dt>FirstName:</dt>
+                        <dd>- {i.firstName}</dd>
+                        <dt>LastName:</dt>
+                        <dd>- {i.lastName}</dd>
+                    </dl>
+                </button>
             ))}
         </div>
     </div>
@@ -78,25 +90,146 @@ function AddUser() {
 function FrontPage() {
 
     return <div>
-        <h1>FRONT PAGE OF DOOM!</h1>
+        <h1>CHAT ROOM OF DOOM!</h1>
         <ul>
-            <li><Link to={"/users"}>List items</Link></li>
-            <li><Link to={"/users/add"}>Add a new item</Link></li>
+            <li><Link to={"/users"}>List users</Link></li>
+            <li><Link to={"/users/add"}>Add a new user</Link></li>
         </ul>
     </div>;
 }
 
+function ListMessages() {
+    const [loading, setLoading] = useState(true);
+    const [message, setMessage] = useState([]);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch("/api/messages/thread/1");
+            setMessage(await data.json());
+            setLoading(false);
+        }
+        fetchData()
+            .catch(console.error)
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    return <div>
+        <Link to={"/"}>Back to home</Link>
+        <div>
+            {message.map((i) => (
+                <ul>
+                    <li>{i.user.username}</li>
+                    <li>{i.body}</li>
+                    <li>{i.sentDate}</li>
+                </ul>
+            ))}
+        </div>
+    </div>
+
+}
+
+function ListThreads({user}) {
+   console.log(user)
+    const [loading, setLoading] = useState(true);
+    const [thread, setThread] = useState([]);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch("/api/users/" + user.id + "/threads");
+            setThread(await data.json());
+            setLoading(false);
+        }
+        fetchData()
+            .catch(console.error)
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    return <div>
+        <h1>Threads your a member of</h1>
+        <Link to={"/"}>Back to home</Link>
+        <div>
+            {thread.map((i) => (
+                <button style={{border: '1px solid black'}}>
+                    <dl>
+                        <dt>ThreadID:</dt>
+                        <dd>- {i.id}</dd>
+                        <dt>Creator:</dt>
+                        <dd>- {i.creator.username}</dd>
+                        <dt>Subject:</dt>
+                        <dd>- {'Subject'}</dd>
+                    </dl>
+                </button>
+            ))}
+        </div>
+    </div>
+
+}
+
+function AddThread() {
+    const navigate = useNavigate()
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [gender, setGender] = useState("");
+
+    async function handleSubmit(e) {
+
+        e.preventDefault();
+        await fetch("/api/users", {
+            method: "post",
+            body: JSON.stringify({firstName, lastName, gender, username}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        navigate("/users")
+    }
+
+    return (
+        <div className="App">
+            <h1>Add item</h1>
+            <Link to={"/"}>Back to home</Link>
+            <form onSubmit={handleSubmit}>
+                <div><label>First Name: <input type="text" onChange={e => setFirstName(e.target.value)}/></label></div>
+                <div><label>Last Name: <input type="text" onChange={e => setLastName(e.target.value)}/></label></div>
+                <div><label>Username: <input type="text" onChange={e => setUsername(e.target.value)}/></label></div>
+                <div><label>Gender: <textarea onChange={e => setGender(e.target.value)}></textarea></label>
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+function AddMessage() {
+    return null;
+}
+
 function App() {
+    const [user, setUser] = useState();
 
     return <HashRouter>
         <Routes>
             <Route path={"/"} element={<FrontPage/>}></Route>
+            <Route path={"/users"} element={<ListUser userId={setUser}/>}></Route>
             <Route path={"/users/add"} element={<AddUser/>}></Route>
-            <Route path={"/users"} element={<ListUser/>}></Route>
+            <Route path={"/threads"} element={<ListThreads user={user}/>}></Route>
+            <Route path={"/threads/add"} element={<AddThread/>}></Route>
+            <Route path={"/messages"} element={<ListMessages/>}></Route>
+            <Route path={"/messages/add"} element={<AddMessage/>}></Route>
+
         </Routes>
     </HashRouter>
 }
-
 
 
 export default App
