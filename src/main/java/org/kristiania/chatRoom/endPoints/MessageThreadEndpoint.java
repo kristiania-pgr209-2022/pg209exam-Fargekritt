1a0af5a78dee66711ea5fcfe4035a2f65ef6d9ca
Rikatob <rikatob@hotmail.com>
2022-11-14 14:20:08 +0100

    Thread end point fixing

--- src/main/java/org/kristiania/chatRoom/endPoints/MessageThreadEndpoint.java
+++ src/main/java/org/kristiania/chatRoom/endPoints/MessageThreadEndpoint.java
@@ -8,6 +8,8 @@ import org.kristiania.chatRoom.database.MessageThreadDao;
 import org.kristiania.chatRoom.database.UserDao;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Path("/thread")
 public class MessageThreadEndpoint {
@@ -22,10 +24,23 @@ public class MessageThreadEndpoint {
     @Path("{id}")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addThread(@PathParam("id") long id, MessageThread thread){
+    @Produces(MediaType.APPLICATION_JSON)
+    public MessageThread addThread(@PathParam("id") long id, MessageThread thread){
         var user = userDao.retrieve(id);
         thread.setCreator(user);
         messageThreadDao.save(thread);
+
+        // set a timeout to check if id gets right.
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(thread.getId());
+            }
+        }, 300L);
+
+
+        System.out.println(thread.getId());
+        return thread;
     }
 
 
